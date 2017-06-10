32537072524d6b78419db47d9d7fdcd190847bbd
linxinzhe <linxinzhechn@icloud.com>
2017-06-10 20:20:53 +0800

    feat(test): minimax and alphabeta unittests

--- agent_test.py
+++ agent_test.py
@@ -9,16 +9,42 @@ import isolation
 import game_agent
 
 from importlib import reload
+from sample_players import *
+from game_agent import *
 
 
-class IsolationTest(unittest.TestCase):
-    """Unit tests for isolation agents"""
+class IsolationMinimaxTest(unittest.TestCase):
+    """Unit tests for Minimax agents"""
 
     def setUp(self):
         reload(game_agent)
-        self.player1 = "Player1"
-        self.player2 = "Player2"
-        self.game = isolation.Board(self.player1, self.player2)
+        self.player1 = RandomPlayer()
+        self.player2 = MinimaxPlayer(search_depth=3, score_fn=open_move_score)
+        self.game = isolation.Board(self.player1, self.player2, width=9, height=9)
+
+    def testMiniMax(self):
+        print(self.game.to_string())
+        winner, history, outcome = self.game.play()
+        print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
+        print(self.game.to_string())
+        print("Move history:\n{!s}".format(history))
+
+
+class IsolationAlphaBetaTest(unittest.TestCase):
+    """Unit tests for AlphaBeta agents"""
+
+    def setUp(self):
+        reload(game_agent)
+        self.player1 = AlphaBetaPlayer(search_depth=2, score_fn=open_move_score)
+        self.player2 = RandomPlayer()
+        self.game = isolation.Board(self.player1, self.player2, width=9, height=9)
+
+    def testAlphaBeta(self):
+        print(self.game.to_string())
+        winner, history, outcome = self.game.play()
+        print("\nWinner: {}\nOutcome: {}".format(winner, outcome))
+        print(self.game.to_string())
+        print("Move history:\n{!s}".format(history))
 
 
 if __name__ == '__main__':
